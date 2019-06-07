9cf63cd393b9ad6b374849ba8bd3fab458f3683b
Katsuki Fujimoto <kf1996cnbl@gmail.com>
2019-06-07 21:46:25 +0900

    Create reservations with datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require popper
 //= require bootstrap-sprockets
 //= require rails-ujs
+//= require jquery-ui/datepicker
 //= require toastr
 //= require activestorage
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
       get 'location'
     end
     resources :photos, only: [:create, :destroy]
+    resources :reservations, only: [:create]
   end
 end
