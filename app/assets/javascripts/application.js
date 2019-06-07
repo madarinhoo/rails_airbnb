d2e47d57645a2f73e8532b2e54dfd24f3c169a19
Katsuki Fujimoto <kf1996cnbl@gmail.com>
2019-06-07 19:02:52 +0900

    Create views for room and show google map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,5 +16,4 @@
 //= require rails-ujs
 //= require toastr
 //= require activestorage
-//= require turbolinks
 //= require_tree .
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_103930) do
+ActiveRecord::Schema.define(version: 2019_06_07_083517) do
 
   create_table "photos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "room_id"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2019_06_02_103930) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
