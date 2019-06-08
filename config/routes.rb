d3f5cd0b2ce7753cce7953c7c13336791f97a2ca
Katsuki Fujimoto <kf1996cnbl@gmail.com>
2019-06-08 11:14:14 +0900

    Create your trips page and your reservations page. Modify user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,7 @@ Rails.application.routes.draw do
     resources :photos, only: [:create, :destroy]
     resources :reservations, only: [:create]
   end
+
+  get '/your_trips' => 'reservations#your_trips'
+  get '/your_reservations' => 'reservations#your_reservations'
 end
