de21588191bfd3076a5b436756722464a5a9010c
Katsuki Fujimoto <kf1996cnbl@gmail.com>
2019-06-09 18:39:31 +0900

    Verify phone number with sms

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,4 +32,23 @@ class User < ApplicationRecord
       end
     end
   end
+
+  def generate_pin
+    self.pin = SecureRandom.hex(2)
+    self.phone_verified = false
+    save
+  end
+
+  def send_pin
+    @client = Twilio::REST::Client.new
+    @client.messages.create(
+      from: ENV['TWILIO_PHONE_NUMBER'],
+      to: self.phone_number,
+      body: "Your pin is #{self.pin}"
+    )
+  end
+
+  def verify_pin(entered_pin)
+    update(phone_verified: true) if self.pin == entered_pin
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_08_021942) do
+ActiveRecord::Schema.define(version: 2019_06_09_083823) do
 
   create_table "photos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "room_id"
@@ -93,6 +93,8 @@ ActiveRecord::Schema.define(version: 2019_06_08_021942) do
     t.string "image"
     t.string "phone_number"
     t.text "description"
+    t.string "pin"
+    t.boolean "phone_verified"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
