96e6be3375e60ab2e5c071a0c69a7a5e89dba99c
Katsuki Fujimoto <kf1996cnbl@gmail.com>
2019-06-12 00:49:04 +0900

    Implement instant/request booking function

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -62,7 +62,7 @@ class RoomsController < ApplicationController
   # RESERVATIONS
   def preload
     today = Date.today
-    reservations = @room.reservations.where('start_date >= ? OR end_date >= ?', today, today)
+    reservations = @room.reservations.where('(start_date >= ? OR end_date >= ?) AND status = ?', today, today, 1)
 
     render json: reservations
   end
@@ -80,7 +80,7 @@ class RoomsController < ApplicationController
 
   private
     def is_conflict(start_date, end_date, room)
-      check = room.reservations.where('? < start_date AND end_date < ?', start_date, end_date)
+      check = room.reservations.where('(? < start_date AND end_date < ?) AND status = ?', start_date, end_date, 1)
       check.size > 0 ? true : false
     end
 
@@ -97,6 +97,6 @@ class RoomsController < ApplicationController
     end
 
     def room_params
-      params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room, :listing_name, :summary, :address, :is_tv, :is_kitchen, :is_air, :is_heating, :is_internet, :price, :active)
+      params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room, :listing_name, :summary, :address, :is_tv, :is_kitchen, :is_air, :is_heating, :is_internet, :price, :active, :instant)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_09_083823) do
+ActiveRecord::Schema.define(version: 2019_06_11_145911) do
 
   create_table "photos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "room_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_06_09_083823) do
     t.integer "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["room_id"], name: "index_reservations_on_room_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
@@ -73,6 +74,7 @@ ActiveRecord::Schema.define(version: 2019_06_09_083823) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.integer "instant", default: 1
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
