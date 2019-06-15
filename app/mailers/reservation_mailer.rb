3e8f3cc3ac417b824a3c65cd76194ec1f8f544f1
Katsuki Fujimoto <kf1996cnbl@gmail.com>
2019-06-15 11:06:48 +0900

    Implement notification function for reservation creation with SMS and Email

---
+++ app/mailers/reservation_mailer.rb
@@ -0,0 +1,7 @@
+class ReservationMailer < ApplicationMailer
+  def send_email_to_guest(guest, room)
+    @recipient = guest
+    @room = room
+    mail(to: @recipient.email, subject: 'Enjoy Your Trip! 😘💋')
+  end
+end
